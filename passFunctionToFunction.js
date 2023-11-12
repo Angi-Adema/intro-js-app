95a985a4bc0e07237d2341145a06b75d77d09da3
CoderGirl <angi.adema@gmail.com>
2023-11-11 17:00:31 -0700

    practice JavaScript

---
+++ passFunctionToFunction.js
@@ -0,0 +1,43 @@
+// Passing a function to another function and calling the original function with a value.
+// function printVariable(variable) {
+//     console.log(variable)
+// }
+
+// function func(x) {
+//     x("hello world")
+// }
+
+// func(printVariable)
+
+// printVariable("Hi")
+
+// Passing a function to another function then passing in values.
+// function sumCallback(a, b, callback) {
+//   callback(a + b)
+// }
+
+// function handleSum(sum) {
+//   console.log(sum)
+// }
+
+// sumCallback(1, 2, handleSum)
+
+// Passing a function to another function and appending a value to the passed in function in order to append this value to the passed in variable.
+// function printVariable(variable) {
+//   console.log(variable)
+// }
+
+// function nameFunction(name, callback) {
+//   callback("Hello " + name)
+// }
+
+// nameFunction("Angi", printVariable)
+
+// Can also pass in the actual traditional function() like this.
+// function nameFunction(name, callback) {
+//   callback("Hello " + name)
+// }
+
+// nameFunction("Angi", function (variable) {
+//   console.log(variable)
+// })
--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// function sum(a, b) {
-//   return a + b
-// }
 
-// let s = sum(2, 2)
-// console.log(s)
-
-// let sum2 = sum(2, s)
-// console.log(sum2)
-
-function name1(name) {
-  return "Hello" + " " + name
-}
-
-let name2 = name1("Angi")
-console.log(name2)
