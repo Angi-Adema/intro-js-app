a74983ef0ddb84087c79f65192ebc1d44a6225e2
CoderGirl <angi.adema@gmail.com>
2023-11-13 16:41:55 -0700

    javascript practice

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const car = {
-  make: "Subaru",
-  model: "CrossTrek",
-  isUsed: false,
-  makeNoise() {
-    console.log("Vroom")
-  },
+const a = [1, 2] // 0x01
+const elementToAdd = 3 // 3
+
+add(a, elementToAdd) // 0x01, 3
+
+console.log(a)
+// console.log(elementToAdd)
+
+function add(array, element) {
+  element = element + 1 // 4
+  array.push(element) // 0x01
 }
+
+const array = a
+array.push(4)
