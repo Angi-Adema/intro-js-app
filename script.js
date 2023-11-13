7eb2db6f185b18e3d462dcca5f1cf00bc74aa7d0
CoderGirl <angi.adema@gmail.com>
2023-11-13 15:44:33 -0700

    javascript practice

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const a = [
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-]
-
-console.log(a[0][3])
-console.log(a[1][2])
-console.log(a[2][0])
+const car = {
+  make: "Subaru",
+  model: "CrossTrek",
+  isUsed: false,
+  makeNoise() {
+    console.log("Vroom")
+  },
+}
