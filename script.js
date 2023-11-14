adf7ec09bcf9c42033bc48b4ff7ed84a50baf5a8
CoderGirl <angi.adema@gmail.com>
2023-11-14 12:11:26 -0700

    added files to practice javascript concepts

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const grandParent = document.querySelector("#grand-parent")
 
-grandParent.style.color = "red"
-
-grandParent.children[0].style.color = "purple"
-
-grandParent.children[0].children[0].style.color = "blue"
-
-grandParent.children[1].style.color = "green"
