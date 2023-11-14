78607dadcfcfc9bf8d64a9aeffce8133453646cc
CoderGirl <angi.adema@gmail.com>
2023-11-14 15:28:51 -0700

    practiced javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const array = ["cat", "dog", "bird", "fish", "lizzard", "spider"]
 
+if (array.length === 0) {
+  console.log("empty")
+} else if (array.length < 5) {
+  console.log("small")
+} else if (array.length < 10) {
+  console.log("medium")
+} else {
+  console.log("large")
+}
