e6242dbb3200b1edd276f18d2bdfc778cf9e82be
CoderGirl <angi.adema@gmail.com>
2023-11-15 08:53:34 -0700

    javascript practice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const number = 9
 
+switch (number) {
+  case 0:
+    console.log("it is zero")
+    break
+  case 1:
+  case 2:
+    console.log("it is small")
+    break
+  case 3:
+  case 4:
+    console.log("it is medium")
+    break
+  case 5:
+    console.log("it is large")
+    break
+  default:
+    console.log("try again")
+}
