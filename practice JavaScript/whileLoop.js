106d3a3a8e8ae00645d9c7b49e377b27d057b8ec
CoderGirl <angi.adema@gmail.com>
2023-11-15 10:41:03 -0700

    javascript practice

---
+++ practice JavaScript/whileLoop.js
@@ -0,0 +1,18 @@
+// const person = {
+//   name: "Kyle",
+//   friend: {
+//     name: "Joe",
+//     friend: {
+//       name: "Sally",
+//     },
+//   },
+// }
+
+// let currentPerson = person
+
+// while (currentPerson.friend != null) {
+//   currentPerson = currentPerson.friend
+//   console.log(currentPerson.name)
+// }
+
+// Only use a while loop when you are not sure how many times we need to loop through an object since we do not know how many items deep the object is.
