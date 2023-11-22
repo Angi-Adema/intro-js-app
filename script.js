2c904075aab1273b9db57dad057150f251046049
CoderGirl <angi.adema@gmail.com>
2023-11-22 13:20:20 -0700

    javascript practice to experiment with asynchronous code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+setTimeout(() => {
+  console.log("inside")
+}, 5000)
 
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+  console.log("clicked")
+})
+
+console.log("outside")
