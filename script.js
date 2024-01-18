1b206e506fb01823302840bb6fa144ea205ae95b
CoderGirl <angi.adema@gmail.com>
2024-01-18 13:31:15 -0700

    practiced fetch promises

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+// fetch("https://jsonplaceholder.typicode.com/users").then(response => {
+//     return response.json()
+// })
+// .then(data => {
+//     console.log(data.map(user => user.name))
+// })
 
+async function doStuff() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json()
+    console.log(data.map(user => user.name))
+}
+doStuff()
 
 
 
