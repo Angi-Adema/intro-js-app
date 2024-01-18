4da7c035121b01461da87eba22bd280b8681cfb3
CoderGirl <angi.adema@gmail.com>
2024-01-18 15:11:48 -0700

    looked at various storage methods

---
+++ practice JavaScript/storage.js
@@ -0,0 +1,8 @@
+// Local Storage   | Cookies              | Session Storage
+// 10MB            | 4KB                  | 5MB
+// Never expires   | Manual Expiration    | Expires on tab close
+// Client          | Client/Server        | Client
+// Easy            | Hard                 | Easy
+
+localStorage.setItem('Name', 'Kyle')
+sessionStorage.setItem('Age', '25')
