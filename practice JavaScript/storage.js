df849cf207c83fedc8f1b58fa46d7995be9570de
CoderGirl <angi.adema@gmail.com>
2024-01-18 15:19:30 -0700

    looked at various types of storage

--- practice JavaScript/storage.js
+++ practice JavaScript/storage.js
@@ -4,5 +4,19 @@
 // Client          | Client/Server        | Client
 // Easy            | Hard                 | Easy
 
-localStorage.setItem('Name', 'Kyle')
-sessionStorage.setItem('Age', '25')
+// localStorage.setItem('Name', 'Kyle')
+// sessionStorage.setItem('Age', '25')
+
+// localStorage.setItem('Name', 'Sally')
+// sessionStorage.setItem('Age', '35')
+
+// Cookies:
+// const date = new Date(9999, 0, 1).toUTCString()
+// const pastDate = new Date(1999, 0, 1).toUTCString()
+// document.cookie = `name=Kyle; expires=${date}`
+// document.cookie = `age=25; expires=${date}`
+// console.log(document.cookie)
+// document.cookie = `name=; expires=${pastDate}`
+// console.log(document.cookie)
+
+
